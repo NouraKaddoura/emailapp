c858e4a74b4915a623d04881c33e771366c23df9
Noura Kaddoura <nourakaddoura@gmail.com>
2018-04-25 14:46:07 -0700

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res)=> {
-    res.send({ hi: 'there'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+
+
+//console.developers.google.com
+
 
 //whenever heroku runs app, enviornment variables
 //look and see if they have declared a port for us to use
